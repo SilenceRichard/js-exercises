2723b75909e2846a0bc5670cb79d21eb8b26758c
Richard <silencerichard@163.com>
2021-01-24 11:30:44 +0800

    :pencil: promise

--- 原理类/promise.js
+++ 原理类/promise.js
@@ -4,72 +4,87 @@
  */
 
 class MyPromise {
+  // const p = new Promise((resolve, reject) => {})
+  // callback: (resolve, reject) => void
   constructor(callback) {
     this.state = 'pending';
     this.value = null;
     this.reason = null;
-    this.resolveArr = [];
-    this.rejectArr = [];
-    callback(this.resolve, this.reject)
-  }
-  resolve = (value) => {
-    // 
-    if (this.state === 'pending') {
-      // do resolve
-      this.state = 'fullfiled';
-      this.value = value;
-      this.resolveArr.forEach(fn => {
-        console.log(this.value)
-        fn(this.value)
-      })
-      // state => fullfiled
+    this.resolveStack = [];
+    this.rejectedStack = [];
+    const resolve = (v) => {
+      if (this.state === 'pending') {
+        this.state = 'fulfilled';
+        this.value = v;
+        this.resolveStack.forEach(fn => {
+          fn(v);
+        })
+      }
     }
+    const reject = (r) => {
+      if (this.state === 'pending') {
+        this.state = 'rejected';
+        this.reason = r;
+        this.rejectedStack.forEach(fn => {
+          fn(r);
+        })
+      }
+    }
+    callback(resolve, reject);
   }
-  reject = (reason) => {
-    //
-    this.state = 'rejected';
-    this.reason = reason;
-    this.resolveArr.forEach(fn => fn(this.reason))
-  }
+  // then: (onFullfiled, onRejected) => Promise((resolve, reject))
   then = (onFullfiled, onRejected) => {
-    //
-    if (this.state === 'fullfiled') {
-      onFullfiled(this.value)
-    }
-    if (this.state === 'rejected') {
-      onRejected(this.value)
-    }
-    if (this.state === 'pending') {
-      return new MyPromise((resolve, reject) => {
-        try {
-          this.resolveArr.push(value => {
-            const result = onFullfiled(value);
+    return new MyPromise((resolve, reject) => {
+      try {
+        if (this.state === 'fulfilled') {
+          const result = onFullfiled(this.value)
+          if (result instanceof MyPromise) {
+            result.then(resolve, reject);
+          } else {
+            resolve(result);
+          }
+        }
+        if (this.state === 'rejected') {
+          const result = onRejected(this.reason);
+          if (result instanceof MyPromise) {
+            result.then(resolve, reject);
+          } else {
+            reject(result);
+          }
+        }
+        // 处理异步
+        if (this.state === 'pending') {
+          this.resolveStack.push((v) => {
+            const result = onFullfiled(v);
             if (result instanceof MyPromise) {
               result.then(resolve, reject);
             } else {
               resolve(result);
             }
-          });
-          this.rejectArr.push(reason => {
-            const result = onRejected(reason);
-            if (result instanceof MyPromise) {
-              result.then(resolve, reject)
-            } else {
-              resolve(result);
-            }
           })
-        } catch (error) {
-          reject(error)
+          this.rejectedStack.push((r) => {
+            const result = onRejected(r);
+            if (result instanceof MyPromise) {
+              result.then(resolve, reject);
+            } else {
+              reject(r);
+            }
+           })
         }
-       
-      })
-    }
+      } catch (e) {
+        reject(e)
+      }
+    })
   }
 }
 
 const p = new MyPromise((resovle, reject) => {
-  reject('error')
-  // resovle('success')
+  console.log('execute callback');
+  // setTimeout(() => {
+  //   resovle('success')
+  //   reject('error')
+  // }, 0)
+  resovle('success')
 })
 
 p.then(v => console.log(v), r => console.log(r))
